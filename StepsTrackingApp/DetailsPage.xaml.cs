2fd4fb2385e314f2736ccc8cd121de667cc8aa07
Adam Anderson <andersad@gmail.com>
2019-10-26 17:47:37 -0400

    Use geolocation for static map

--- StepsTrackingApp/DetailsPage.xaml.cs
+++ StepsTrackingApp/DetailsPage.xaml.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,11 +13,33 @@ namespace StepsTrackingApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetailsPage : ContentPage
     {
+        private CancellationTokenSource getLocationTokenSource;
+
         public DetailsPage()
         {
             InitializeComponent();
+            this.Appearing += this.DetailsPage_Appearing;
+            this.Disappearing += this.DetailsPage_Disappearing;
+        }
 
-            this.StaticMapImage.Source = $"https://api.mapbox.com/styles/v1/mapbox/light-v9/static/-82.9332,40.1261,13.58,0,0/300x200@2x?access_token={ApiKeys.MAPBOX_API_KEY}";
+        private void DetailsPage_Disappearing(object sender, EventArgs e)
+        {
+            if (getLocationTokenSource != null && !getLocationTokenSource.IsCancellationRequested)
+            {
+                getLocationTokenSource.Cancel();
+            }
+        }
+
+        private async void DetailsPage_Appearing(object sender, EventArgs e)
+        {
+            var location = await Geolocation.GetLastKnownLocationAsync();
+            if (location == null)
+            {
+                this.getLocationTokenSource = new CancellationTokenSource(30000);
+                var request = new GeolocationRequest(GeolocationAccuracy.Low);
+                location = await Geolocation.GetLocationAsync(request, getLocationTokenSource.Token);
+            }
+            this.StaticMapImage.Source = $"https://api.mapbox.com/styles/v1/mapbox/light-v9/static/{location.Longitude},{location.Latitude},13.58,0,0/300x200@2x?access_token={ApiKeys.MAPBOX_API_KEY}";
         }
 
         private async void MenuTapGestureRecognizer_Tapped(object sender, EventArgs e)
